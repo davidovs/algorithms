0f13dbc27db6da1be68ac3bd5ca181998007e8d0
Sergey_Davidov <Sergey_Davidov@epam.com>
2019-07-24 14:38:16 +0300

    import static method

--- src/test/java/task2/SolutionTest.java
+++ src/test/java/task2/SolutionTest.java
@@ -1,7 +1,8 @@
 package task2;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
@@ -9,51 +10,51 @@ public class SolutionTest {
 
     @Test
     public void test1() {
-        Assert.assertEquals(solution.execute(new int[]{1, 2, 0}), 3);
+        assertEquals(solution.execute(new int[]{1, 2, 0}), 3);
     }
 
     @Test
     public void test2() {
-        Assert.assertEquals(solution.execute(new int[]{3, 4, -1, 1}), 2);
+        assertEquals(solution.execute(new int[]{3, 4, -1, 1}), 2);
     }
 
     @Test
     public void test3() {
-        Assert.assertEquals(solution.execute(new int[]{7, 8, 9, 11, 12}), 1);
+        assertEquals(solution.execute(new int[]{7, 8, 9, 11, 12}), 1);
     }
 
     @Test
     public void test4() {
-        Assert.assertEquals(solution.execute(new int[]{5, -3, 2, 7, -4, 0, 1}), 3);
+        assertEquals(solution.execute(new int[]{5, -3, 2, 7, -4, 0, 1}), 3);
     }
 
     @Test
     public void test5() {
-        Assert.assertEquals(solution.execute(new int[]{1, 2, 3, 4}), 5);
+        assertEquals(solution.execute(new int[]{1, 2, 3, 4}), 5);
     }
 
     @Test
     public void test6() {
-        Assert.assertEquals(solution.execute(new int[]{4, 3, 2, 1}), 5);
+        assertEquals(solution.execute(new int[]{4, 3, 2, 1}), 5);
     }
 
     @Test
     public void test7() {
-        Assert.assertEquals(solution.execute(new int[]{1, -2, 3, -4}), 2);
+        assertEquals(solution.execute(new int[]{1, -2, 3, -4}), 2);
     }
 
     @Test
     public void test8() {
-        Assert.assertEquals(solution.execute(new int[]{-4, -3, -2, -1}), 1);
+        assertEquals(solution.execute(new int[]{-4, -3, -2, -1}), 1);
     }
 
     @Test
     public void test9() {
-        Assert.assertEquals(solution.execute(new int[]{0, -3, 2, -1}), 1);
+        assertEquals(solution.execute(new int[]{0, -3, 2, -1}), 1);
     }
 
     @Test
     public void test10() {
-        Assert.assertEquals(solution.execute(new int[]{2, 1, 2, 0}), 3);
+        assertEquals(solution.execute(new int[]{2, 1, 2, 0}), 3);
     }
 }
